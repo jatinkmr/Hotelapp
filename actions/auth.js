5bb8ef442c0b61e9d04dced9df2ab31a2bdb33c4
Jatin Kamboj <kumar.jatin873@gmail.com>
2020-03-02 17:22:19 +0530

    login action created

--- actions/auth.js
+++ actions/auth.js
@@ -36,4 +36,30 @@ const registration = async (req, res) => {
     }
 };
 
+const login = async (req, res) => {
+    const { error } = loginValidation(req.body);
+    if(error) {
+        return res.status(400).send(error.details[0].message);
+    }
+    
+    const user = await User.findOne({
+        email: req.body.email
+    });
+
+    if(!user) {
+        return res.status(400).send('Email Does Not Exists !!');
+    }
+
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if(!validPass) {
+        return res.status(400).send('Invalid Password !!');
+    }
+
+    const token = jwt.sign({
+        _id: user._id
+    }, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
+};
+
 module.exports.registration = registration;
+module.exports.login = login;
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
-const User = require('../model/User');
-const { registerValidation, loginValidation } = require('../validation');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const { notFound } = require("@hapi/boom");
-const { registration } = require('../actions/auth');
+const { registration, login } = require('../actions/auth');
 
 router.post("/register", async (req, res, next) => {
     try{
@@ -16,28 +12,12 @@ router.post("/register", async (req, res, next) => {
 });
 
 router.post('/login', async (req, res) => {
-    const { error } = loginValidation(req.body);
-    if(error) {
-        return res.status(400).send(error.details[0].message);
+    try {
+        const response = await login(req, res);
+        return res.send(JSON.stringify(response));
+    } catch(err) {
+        next(err);
     }
-    
-    const user = await User.findOne({
-        email: req.body.email
-    });
-
-    if(!user) {
-        return res.status(400).send('Email Does Not Exists !!');
-    }
-
-    const validPass = await bcrypt.compare(req.body.password, user.password);
-    if(!validPass) {
-        return res.status(400).send('Invalid Password !!');
-    }
-
-    const token = jwt.sign({
-        _id: user._id
-    }, process.env.TOKEN_SECRET);
-    res.header('auth-token', token).send(token);
 });
 
 router.all("*", (req, res, next) => {
