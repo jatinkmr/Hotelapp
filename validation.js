8b6bf2bb2713c4148d7e229f29bae2c32dddfde2
Jatin Kamboj <kumar.jatin873@gmail.com>
2020-03-09 21:58:52 +0530

    user role added

--- validation.js
+++ validation.js
@@ -4,7 +4,8 @@ const registerValidation = data => {
     const schema = Joi.object().keys({
         name: Joi.string().min(6).required(),
         email: Joi.string().min(8).required().email(),
-        password: Joi.string().min(6).required()
+        password: Joi.string().min(6).required(),
+        role: Joi.string().min(8).required()
     });
     return Joi.validate(data, schema);
 };
