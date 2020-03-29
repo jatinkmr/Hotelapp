96a24988ea00dae605ffa2da0ae938a261aa57f4
Jatin Kamboj <kumar.jatin873@gmail.com>
2020-03-29 17:03:51 +0530

    Booking Home Page Show Informations of Both Types of Users

--- actions/booking.js
+++ actions/booking.js
@@ -1,4 +1,5 @@
 const User = require('../model/User');
+const Hotel = require('../model/hotel');
 const Boom = require('@hapi/boom');
 const jwt = require('jsonwebtoken');
 
@@ -20,13 +21,28 @@ const bookingHome = async (req, res, next) => {
 
             User.findOne({_id: userId}).then(users => {
                 console.log('User => ', users);
+                if (users.role.toLowerCase() == 'hotelowner') {
+                    // console.log('You are HotelOwner !!');
+
+                    Hotel.find({ownerID: userId}).then(hotels => {
+                        // console.log('Hotels => ', hotels);
+                        return res.json({message: 'Welcome to Home Page !!', userData: users, hotelData: hotels});
+                    }).catch(err => {
+                        // console.log('error =>', err);
+                        return res.json(Boom.notFound('Your Hotels Not Found !!').output.payload.message);
+                    });
+                } else {
+                    // console.log('You are Customer !!');
+                    return res.json({message: 'Welcome to Home Page !!', userData: users});
+                }
+                // return res.json({message: 'Welcome to Home Page', data: users});
             }).catch(err => {
-                console.log('Error =>', err);
+                // console.log('Error =>', err);
                 return res.json(Boom.notFound('User Not Found !!').output.payload.message);
             })
         }catch(err) {
             // console.log('Error');
-            return res.json(Boom.unauthorized('Unauthorized'));
+            return res.json(Boom.unauthorized('Unauthorized').output.payload.message);
         }
     }
     return res.status(500);
